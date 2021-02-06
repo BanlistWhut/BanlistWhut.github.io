84229d82632b9bdc4eb5ebdd43910073d18631e9
banlist <46648920+BanlistWhut@users.noreply.github.com>
2021-02-06 22:53:17 +0800

    增加文档

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -1,57 +1,82 @@
 module.exports = [
-  { text: '首页', link: '/' },
-  { text: '计算机', link: '/computer/' },
-  { text: '系统', items: [
-    { text: 'Linux', link: '/os/linux/' },
-    { text: 'Manjaro', link: '/os/manjaro/' },
-    { text: 'Ubuntu', link: '/os/ubuntu/' },
-    { text: 'CentOS', link: '/os/centos/' },
-  ]},
-  { text: '前端', items: [
-    { text: 'JavaScript', link: '/frontend/javascript/' },
-    { text: 'CSS', link: '/frontend/css/' },
-    { text: 'Vue', link: '/frontend/vue/' },
-    { text: 'Webpack', link: '/frontend/webpack/' },
-    { text: 'Flutter', link: '/frontend/flutter/' },
-    { text: 'D3', link: '/frontend/d3js/' },
-    { text: 'Utils', link: '/frontend/utils/' },
-  ]},
-  { text: '后端', items: [
-    { text: 'Java', link: '/backend/java/' },
-    { text: '数据库', link: '/backend/database/' },
-    { text: 'Nodejs', link: '/backend/nodejs/' },
-    { text: 'Koa', link: '/backend/koa/' },
-    { text: 'Nginx', link: '/backend/nginx/' },
-    { text: 'MongoDB', link: '/backend/mongodb/' },
-    { text: 'Docker', link: '/backend/docker/' },
-  ]},
-  { text: '开发工具', items: [
-    { text: 'Git', link: '/tools/git/' },
-    { text: 'Github', link: '/tools/github/' },
-    { text: 'VSCode', link: '/tools/vscode/' },
-    { text: 'Chrome Developer tools', link: '/tools/chrome/' },
-    { text: 'Bookmark scripts', link: '/tools/bookmark-scripts/' },
-  ]},
-  { text: '微服务架构2.0-撸帝', items: [
-      { text: '1.什么是微服务', link: '/微服务架构2.0-撸帝/1.什么是微服务/' },
-      { text: '2.linux', link: '/微服务架构2.0-撸帝/2.linux/' },
-      { text: '3.Docker', link: '/微服务架构2.0-撸帝/3.Docker/' },
-      { text: '4.Dockerfile', link: '/微服务架构2.0-撸帝/4.Dockerfile/' },
-      { text: '5.Docker Compose', link: '/微服务架构2.0-撸帝/5.Docker Compose/' },
-      { text: '6.再谈微服务', link: '/微服务架构2.0-撸帝/6.再谈微服务/' },
-      { text: '7.Spring Boot', link: '/微服务架构2.0-撸帝/7.Spring Boot/' },
-      { text: '8.Spring Cloud Alibaba', link: '/微服务架构2.0-撸帝/8.Spring Cloud Alibaba/' },
-      { text: '9.Spring Cloud Alibaba Dubbo', link: '/微服务架构2.0-撸帝/9.Spring Cloud Alibaba Dubbo/' },
-      { text: '10.Vue', link: '/微服务架构2.0-撸帝/10.Vue/' },
-      { text: '11.Spring Security oAuth2', link: '/微服务架构2.0-撸帝/11.Spring Security oAuth2/' },
-      { text: '12.Kubernetes', link: '/微服务架构2.0-撸帝/12.Kubernetes/' },
-      { text: '13.编程方法论', link: '/微服务架构2.0-撸帝/13.编程方法论/' },
-      { text: '14.项目实战 MyShopPlus', link: '/微服务架构2.0-撸帝/14.项目实战 MyShopPlus/' },
-    ]},
-  { text: '黑苹果', link: '/hackintosh/' },
-  { text: '更多', items: [
-    { text: '面试题', link: '/more/interview/' },
-    { text: '小工具', link: '/more/hodgepodge/' },
-    { text: 'VuePress 官网', link: 'https://vuepress.vuejs.org/zh/' },
-  ]}
+  {text: '首页', link: '/'},
+  {text: '计算机', link: '/computer/'},
+  {
+    text: '系统', items: [
+      {text: 'Linux', link: '/os/linux/'},
+      {text: 'Manjaro', link: '/os/manjaro/'},
+      {text: 'Ubuntu', link: '/os/ubuntu/'},
+      {text: 'CentOS', link: '/os/centos/'},
+    ]
+  },
+  {
+    text: '前端', items: [
+      {text: 'JavaScript', link: '/frontend/javascript/'},
+      {text: 'CSS', link: '/frontend/css/'},
+      {text: 'Vue', link: '/frontend/vue/'},
+      {text: 'Webpack', link: '/frontend/webpack/'},
+      {text: 'Flutter', link: '/frontend/flutter/'},
+      {text: 'D3', link: '/frontend/d3js/'},
+      {text: 'Utils', link: '/frontend/utils/'},
+    ]
+  },
+  {
+    text: '后端', items: [
+      {text: 'Java', link: '/backend/java/'},
+      {text: '数据库', link: '/backend/database/'},
+      {text: 'Nodejs', link: '/backend/nodejs/'},
+      {text: 'Koa', link: '/backend/koa/'},
+      {text: 'Nginx', link: '/backend/nginx/'},
+      {text: 'MongoDB', link: '/backend/mongodb/'},
+      {text: 'Docker', link: '/backend/docker/'},
+    ]
+  },
+  {
+    text: '开发工具', items: [
+      {text: 'Git', link: '/tools/git/'},
+      {text: 'Github', link: '/tools/github/'},
+      {text: 'VSCode', link: '/tools/vscode/'},
+      {text: 'Chrome Developer tools', link: '/tools/chrome/'},
+      {text: 'Bookmark scripts', link: '/tools/bookmark-scripts/'},
+    ]
+  },
+  {
+    text: '微服务架构2.0-撸帝', items: [
+      {text: '1.什么是微服务', link: '/微服务架构2.0-撸帝/1.什么是微服务/'},
+      {text: '2.linux', link: '/微服务架构2.0-撸帝/2.linux/'},
+      {text: '3.Docker', link: '/微服务架构2.0-撸帝/3.Docker/'},
+      {text: '4.Dockerfile', link: '/微服务架构2.0-撸帝/4.Dockerfile/'},
+      {text: '5.Docker Compose', link: '/微服务架构2.0-撸帝/5.Docker Compose/'},
+      {text: '6.再谈微服务', link: '/微服务架构2.0-撸帝/6.再谈微服务/'},
+      {text: '7.Spring Boot', link: '/微服务架构2.0-撸帝/7.Spring Boot/'},
+      {text: '8.Spring Cloud Alibaba', link: '/微服务架构2.0-撸帝/8.Spring Cloud Alibaba/'},
+      {text: '9.Spring Cloud Alibaba Dubbo', link: '/微服务架构2.0-撸帝/9.Spring Cloud Alibaba Dubbo/'},
+      {text: '10.Vue', link: '/微服务架构2.0-撸帝/10.Vue/'},
+      {text: '11.Spring Security oAuth2', link: '/微服务架构2.0-撸帝/11.Spring Security oAuth2/'},
+      {text: '12.Kubernetes', link: '/微服务架构2.0-撸帝/12.Kubernetes/'},
+      {text: '13.编程方法论', link: '/微服务架构2.0-撸帝/13.编程方法论/'},
+      {text: '14.项目实战 MyShopPlus', link: '/微服务架构2.0-撸帝/14.项目实战 MyShopPlus/'},
+    ]
+  },
+  {text: '黑苹果', link: '/hackintosh/'},
+  {
+    text: '玩客云', items: [
+      {text: '在线终端', link: 'http://192.168.3.100:7681/'},
+      {text: 'filebrowser', link: 'http://192.168.3.100:9000/'},
+      {
+        text: 'Aria2',
+        link: 'http://ariang.js.org/#!/settings/rpc/set/ws/192.168.3.100/6800/jsonrpc/Yjg4YmYxYmNhMGE0ODYxMDVhMGE='
+      },
+      {text: 'Transmission', link: 'http://192.168.3.100:9091/'},
+
+    ]
+  },
+  {
+    text: '更多', items: [
+      {text: '菜鸟教程', link: 'https://www.runoob.com/'},
+      {text: '面试题', link: '/more/interview/'},
+      {text: '小工具', link: '/more/hodgepodge/'},
+      {text: 'VuePress 官网', link: 'https://vuepress.vuejs.org/zh/'},
+    ]
+  }
 ];

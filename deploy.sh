59369b6287ad36aeddb06e658e06ef195ee85ddd
banlist <46648920+BanlistWhut@users.noreply.github.com>
2021-02-08 11:51:30 +0800

    增加文档

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,8 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:BanlistWhut/BanlistWhut.github.io.git master
 
+git push -f git@gitee.com:yan_tuo/BanlistWhut.github.io.git master
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
